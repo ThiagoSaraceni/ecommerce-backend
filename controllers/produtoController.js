14028c61d54db7099edccde0f1382abcec122df5
ThiagoSaraceni <thiagorx123@gmail.com>
2024-07-30 23:22:37 -0300

    feat: criando rotas crud

--- controllers/produtoController.js
+++ controllers/produtoController.js
@@ -1,7 +1,7 @@
 const Produto = require("../models/product");
 
 const produtoController = {
-  criarProduto: async (req, res) => {
+  createProduct: async (req, res) => {
     console.log("requisicao recebida com sucesso");
 
     const { nome, descricao, preco, url_img, categoria } = req.body;
@@ -17,6 +17,81 @@ const produtoController = {
     } catch (error) {
       console.error("Erro ao criar produto: ", error);
       res.status(500).json({ error: "Erro ao criar produto" });
+    }
+  },
+
+  getProducts: async (req, res) => {
+    console.log("requisicao recebida com sucesso!");
+
+    try {
+      const products = await Produto.findAll();
+      res.status(200).json(products);
+    } catch (error) {
+      console.error("Erro ao buscar produtos ", error);
+      res.status(500).json({ error: "Erro ao buscar produtos" });
+    }
+  },
+
+  getProductById: async (req, res) => {
+    const id = req.params.id;
+    console.log(`Requisicao recebida para o produto com ID: ${id}`);
+
+    try {
+      const products = await Produto.findByPk(id);
+
+      if (!products) {
+        return res.status(404).json({ error: "Produto não encontrado" });
+      }
+
+      res.status(200).json(products);
+    } catch (error) {
+      console.error("Erro ao buscar produto: ", error);
+      res.status(500).json({ error: "Erro ao buscar produto" });
+    }
+  },
+
+  updateProductById: async (req, res) => {
+    const { id } = req.params;
+    const { nome, descricao, preco, url_img, categoria } = req.body;
+    console.log(`Requisicao recebida para o produto com ID: ${id}`);
+
+    try {
+      const product = await Produto.findByPk(id);
+
+      //return para nao ir a linha debaixo
+      if (!product) {
+        return res.status(404).json({ error: "Produto não encontrado" });
+      }
+
+      await product.update({
+        nome,
+        descricao,
+        preco,
+        url_img,
+        categoria,
+      });
+
+      res.status(200).json(product);
+    } catch (error) {
+      console.error("Erro ao atualizar produto: ", error);
+      res.status(500).json({ error: "Erro ao atualizar produto" });
+    }
+  },
+
+  deleteProductById: async (req, res) => {
+    const { id } = req.params;
+    console.log(`Requisicao recebida para deletar o produto com ID: ${id}`);
+    try {
+      const product = await Produto.findByPk(id);
+
+      if (!product) {
+        return res.status(404).json({ error: "Produto não encontrado" });
+      }
+      await product.destroy();
+      res.status(204).send();
+    } catch (error) {
+      console.error("Erro ao deletar produto: ", error);
+      res.status(500).json({ error: "Erro ao deletar produto" });
     }
   },
 };
--- routes/ecommerceRoutes.js
+++ routes/ecommerceRoutes.js
@@ -3,6 +3,10 @@ const produtoController = require("../controllers/produtoController");
 
 const router = express.Router();
 
-router.post("/produtos", produtoController.criarProduto);
+router.post("/produtos", produtoController.createProduct);
+router.get("/produtos", produtoController.getProducts);
+router.get("/produtos/:id", produtoController.getProductById);
+router.put("/produtos/:id", produtoController.updateProductById);
+router.delete("/produto/:id", produtoController.deleteProductById);
 
 module.exports = router;
